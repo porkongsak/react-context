78aae445d7b4f657225705a972e88e8c6c18d0ba
Kongsak <death01_por@hotmail.com>
2021-12-07 03:15:30 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useState } from 'react'
 
-function App() {
+const initialState = {
+  todos: [],
+  todoCount:0
+}
+
+function reducer(state, action ){
+ switch (action.type) {
+   case "ADD_TODO":
+     return {
+       todos: [...state.todos, { text: action.payload, completed: false}],
+       todoCount: state.todoCount + 1
+     };
+   case "TOGGLE_TODO":
+      return {
+        todos: state.todos.map((t, idx) =>
+          idx === action.idx ? { ...t, completed: !t.completed} : t
+        ),
+        todoCount: state.todoCount
+      };
+
+    default:
+      return state;
+ }
+}
+
+const App = () => {
+
+  const [{todos, todoCount}, dispatch ] = useReducer(reducer,initialState); 
+  const [text, setText] = useState();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div >
+      <h3>{todoCount}</h3>
+      <form onSubmit={e =>{
+        e.preventDefault();
+        dispatch({ type: "ADD_TODO", payload: text });
+        setText("");
+      }}>
+
+        <input value={text} onChange={e => setText(e.target.value)}/>
+      </form>
+      {todos.map((t, idx) =>(
+        <div 
+        key={t.text} 
+        onClick={() => dispatch({ type: "TOGGLE_TODO", idx })}
+        style={{
+          textDecoration: t.completed ? "line-through" : ""
+        }}
         >
-          Learn React
-        </a>
-      </header>
+          {t.text}
+        </div>
+      ))}
+      <pre>
+        {JSON.stringify(todos, null, 2)}
+      </pre>
     </div>
   );
 }
